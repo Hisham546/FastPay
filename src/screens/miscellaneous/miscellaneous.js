995c3aadb6f89c225344b5a20b9ab121e44076b8
Hisham <hmuhammed546@gmail.com>
2023-11-01 18:09:56 +0530

    added option icons in miscs

--- src/screens/miscellaneous/miscellaneous.js
+++ src/screens/miscellaneous/miscellaneous.js
@@ -13,7 +13,18 @@ import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 export default function Miscellaneous() {
     return (
         <View style={styles.container}>
-            <View style={styles.miscView1}></View>
+            <View style={styles.miscView1}>
+                <MaterialIcon name="web" color={'#7FC8A9'} size={25} />
+                <MaterialIcon name="water" color={'#61A3BA'} size={25} />
+                <MaterialIcon name="flash" color={'#F9B572'} size={25} />
+                <MaterialIcon name="television" color={'#F875AA'} size={25} />
+            </View>
+            <View style={styles.miscView1}>
+                <MaterialIcon name="car-estate" color={'#186F65'} size={25} />
+                <MaterialIcon name="home" color={'#7FC8A9'} size={25} />
+                <MaterialIcon name="cash-100" color={'#F875AA'} size={25} />
+                <MaterialIcon name="dots-horizontal-circle" color={'#706233'} size={25} />
+            </View>
         </View>
     )
 
--- src/screens/miscellaneous/styles.js
+++ src/screens/miscellaneous/styles.js
@@ -9,14 +9,14 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         marginLeft: '3%',
         borderRadius: 3,
-        // alignItems:'center',
-        flexDirection: 'row',
-        // justifyContent:'space-around'
+
     },
     miscView1: {
         width: deviceWidth * 0.93,
         height: deviceHeight * 0.10,
-        backgroundColor:'blue',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        alignItems:'center'
     },
     miscView2: {
 

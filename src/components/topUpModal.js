bd66c1969c6e672089091b82ebc9fe2c2faa97fc
Hisham <hmuhammed546@gmail.com>
2023-11-03 12:39:09 +0530

    sticked to defaul modal of react native

--- src/components/topUpModal.js
+++ src/components/topUpModal.js
@@ -1,33 +1,85 @@
-import React, { useRef,useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Modalize } from 'react-native-modalize';
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import React, { useRef, useEffect } from 'react';
+import { View, Text, TouchableOpacity,Modal, StyleSheet,Pressable } from 'react-native';
+//import BottomSheet from '@gorhom/bottom-sheet';
 export const TopUpModal = ({ isTopUpOpen }) => {
     //const modalizeRef = useRef < Modalize > (null);
 
-    const modalizeRef = useRef(null);
+    // const bottomSheetRef  = useRef(null);
 
-    useEffect(() => {
-        if (isTopUpOpen) {
-            modalizeRef.current?.open();
-        } else {
-            modalizeRef.current?.close();
-        }
-    }, [isTopUpOpen]);
+    // useEffect(() => {
+    //     if (isTopUpOpen) {
+    //         bottomSheetRef.current?.open();
+    //     } else {
+    //         bottomSheetRef.current?.close();
+    //     }
+    // }, [isTopUpOpen]);
     return (
-        <>
-
-<GestureHandlerRootView style={{ flex: 1 }}>
-            <Modalize ref={modalizeRef}>
-                <View><Text>Topup</Text></View>
-
-            </Modalize>
-            </GestureHandlerRootView>
-        </>
-    );
-    const styles = StyleSheet.create({
-
-
-    })
-
-};
+     
+            <View style={styles.centeredView}>
+              <Modal
+                animationType="fade"
+                transparent={true}
+                visible={isTopUpOpen}
+                onRequestClose={() => {
+                  Alert.alert('Modal has been closed.');
+                  setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                    <Text style={styles.modalText}>Hello World!</Text>
+                    <Pressable
+                      style={[styles.button, styles.buttonClose]}
+                      onPress={() => !isTopUpOpen}>
+                      <Text style={styles.textStyle}>Hide Modal</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </Modal>
+            
+            </View>
+          );
+        };
+        
+        const styles = StyleSheet.create({
+          centeredView: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 22,
+          },
+          modalView: {
+            margin: 20,
+            backgroundColor: 'white',
+            borderRadius: 20,
+            padding: 35,
+            alignItems: 'center',
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+          },
+          button: {
+            borderRadius: 20,
+            padding: 10,
+            elevation: 2,
+          },
+          buttonOpen: {
+            backgroundColor: '#F194FF',
+          },
+          buttonClose: {
+            backgroundColor: '#2196F3',
+          },
+          textStyle: {
+            color: 'white',
+            fontWeight: 'bold',
+            textAlign: 'center',
+          },
+          modalText: {
+            marginBottom: 15,
+            textAlign: 'center',
+          },
+        });
